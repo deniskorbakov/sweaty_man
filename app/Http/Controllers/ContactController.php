5f736fa0924675cfbfa96fec45568775be073109
deniskorbakov <korbakovd@gmail.com>
2023-01-10 15:50:25 +0300

    получаем данные с бд

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
     public function submit(ContactRequest $request) {
+        $contact = new Contact();
+        $contact->userName = $request->input('userName');
+        $contact->userEmail = $request->input('userEmail');
+        $contact->userText = $request->input('userText');
+
+        $contact->save();
+
+        return redirect()->route('home');
 
     }
 }
