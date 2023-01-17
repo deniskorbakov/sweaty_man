79572ff3c2151767af607e8b2f54908f3d95b215
deniskorbakov <korbakovd@gmail.com>
2023-01-17 21:28:11 +0300

    добавил тестовый текст на страницу о нас

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,65 +5,75 @@
 
 @section('content')
     <!--Section: FAQ-->
-    <section>
-        <h3 class="text-center mb-4 pb-2 text-primary fw-bold">FAQ</h3>
-        <p class="text-center mb-5">
-            Find the answers for the most frequently asked questions below
-        </p>
+    <div class="container">
+        <h3 class="text-center mb-4 pb-2 text-black fw-bold mt-5">Ответы на часто задаваемые вопросы</h3>
+            <div class="row mt-5">
+                <div class="col-md-6 col-lg-4 mb-4">
+                    <h4 class="mb-3 text-black">О чем проекте</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Amet at dolore excepturi illo illum, in
+                        odio provident voluptatem voluptates.
+                        Aperiam deleniti enim eveniet ipsum mollitia
+                        nam perspiciatis quos repudiandae suscipit!
+                    </p>
+                </div>
 
-        <div class="row">
-            <div class="col-md-6 col-lg-4 mb-4">
-                <h6 class="mb-3 text-primary"><i class="far fa-paper-plane text-primary pe-2"></i> A simple
-                    question?</h6>
-                <p>
-                    <strong><u>Absolutely!</u></strong> We work with top payment companies which guarantees
-                    your safety and
-                    security. All billing information is stored on our payment processing partner.
-                </p>
-            </div>
+                <div class="col-md-6 col-lg-4 mb-4">
+                    <h4 class="mb-3 text-black">Кто разрабатывал</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Amet at dolore excepturi illo illum, in
+                        odio provident voluptatem voluptates.
+                        Aperiam deleniti enim eveniet ipsum mollitia
+                        nam perspiciatis quos repudiandae suscipit!
+                    </p>
+                </div>
 
-            <div class="col-md-6 col-lg-4 mb-4">
-                <h6 class="mb-3 text-primary"><i class="fas fa-pen-alt text-primary pe-2"></i> A question
-                    that is longer then the previous one?</h6>
-                <p>
-                    <strong><u>Yes, it is possible!</u></strong> You can cancel your subscription anytime in
-                    your account. Once the subscription is
-                    cancelled, you will not be charged next month.
-                </p>
-            </div>
+                <div class="col-md-6 col-lg-4 mb-4">
+                    <h4 class="mb-3 text-black">Кому он может помочь</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Amet at dolore excepturi illo illum, in
+                        odio provident voluptatem voluptates.
+                        Aperiam deleniti enim eveniet ipsum mollitia
+                        nam perspiciatis quos repudiandae suscipit!
+                    </p>
+                </div>
 
-            <div class="col-md-6 col-lg-4 mb-4">
-                <h6 class="mb-3 text-primary"><i class="fas fa-user text-primary pe-2"></i> A simple
-                    question?</h6>
-                <p>
-                    Currently, we only offer monthly subscription. You can upgrade or cancel your monthly
-                    account at any time with no further obligation.
-                </p>
-            </div>
+                <div class="col-md-6 col-lg-4 mb-4">
+                    <h4 class="mb-3 text-black">Главная цель проекта</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Amet at dolore excepturi illo illum, in
+                        odio provident voluptatem voluptates.
+                        Aperiam deleniti enim eveniet ipsum mollitia
+                        nam perspiciatis quos repudiandae suscipit!
+                    </p>
+                </div>
 
-            <div class="col-md-6 col-lg-4 mb-4">
-                <h6 class="mb-3 text-primary"><i class="fas fa-rocket text-primary pe-2"></i> A simple
-                    question?</h6>
-                <p>
-                    Yes. Go to the billing section of your dashboard and update your payment information.
-                </p>
-            </div>
+                <div class="col-md-6 col-lg-4 mb-4">
+                    <h4 class="mb-3 text-black">Платно ли это</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Amet at dolore excepturi illo illum, in
+                        odio provident voluptatem voluptates.
+                        Aperiam deleniti enim eveniet ipsum mollitia
+                        nam perspiciatis quos repudiandae suscipit!
+                    </p>
+                </div>
 
-            <div class="col-md-6 col-lg-4 mb-4">
-                <h6 class="mb-3 text-primary"><i class="fas fa-home text-primary pe-2"></i> A simple
-                    question?</h6>
-                <p><strong><u>Unfortunately no</u>.</strong> We do not issue full or partial refunds for any
-                    reason.</p>
+                <div class="col-md-6 col-lg-4 mb-4">
+                    <h4 class="mb-3 text-black">Если я заметил ошибку на сайте</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Amet at dolore excepturi illo illum, in
+                        odio provident voluptatem voluptates.
+                        Aperiam deleniti enim eveniet ipsum mollitia
+                        nam perspiciatis quos repudiandae suscipit!
+                    </p>
+                </div>
             </div>
-
-            <div class="col-md-6 col-lg-4 mb-4">
-                <h6 class="mb-3 text-primary"><i class="fas fa-book-open text-primary pe-2"></i> Another
-                    question that is longer than usual</h6>
-                <p>
-                    Of course! We’re happy to offer a free plan to anyone who wants to try our service.
-                </p>
-            </div>
-        </div>
-    </section>
+    </div>
     <!--Section: FAQ-->
 @endsection
