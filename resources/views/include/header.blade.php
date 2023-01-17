1c423ea28b17acf6a53e1b12b47f726d0551f7ad
deniskorbakov <korbakovd@gmail.com>
2023-01-17 21:44:21 +0300

    изменил название

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -13,7 +13,7 @@
                     <a class="nav-link active" aria-current="page" href="{{route('home')}}">Главная</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('about')}}">Мы</a>
+                    <a class="nav-link" href="{{route('about')}}">О нас</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('contact')}}">Контактная форма</a>
