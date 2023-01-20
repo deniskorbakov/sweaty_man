83a83f2cee585ba92179d0ea9c9d0c10bd6014be
deniskorbakov <korbakovd@gmail.com>
2023-01-21 01:05:49 +0300

    проверка на юзера

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -27,6 +28,8 @@ class RegisterController extends Controller
             'email' => $request->emailReg,
             'password' => Hash::make($request->passwordReg)
         ]);
+
+        event(new Registered($user));
 
         Auth::login($user);
 
