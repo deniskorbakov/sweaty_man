97e759108a8c247a6d3231bb8404ac8c996c3193
deniskorbakov <korbakovd@gmail.com>
2023-01-21 01:05:30 +0300

    логика отправки и подтверждения почты

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ForgotController;
 use App\Http\Controllers\ResetPasswordController;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 
 /*
@@ -48,13 +50,43 @@ Route::get('/authorization', [RegisterController::class,'create'])->middleware('
 Route::post('/register', [RegisterController::class,'store'])->middleware('guest')->name('register-store');
 Route::post('/login', [LoginController::class,'store'])->middleware('guest')->name('login-store');
 
-Route::view('/userAccount','userAccount')->middleware('auth')->name('userAccount');
-
 Route::view('/forgotPassword','forgotPassword')->middleware('guest')->name('forgotPassword');
 Route::post('/forgotPass', [ForgotController::class,'forgot'])->middleware('guest')->name('forgotPass');
 
 Route::get('/reset-password', [ResetPasswordController::class,'create'])->middleware('guest')->name('password.reset');
 Route::post('/reset-pass', [ResetPasswordController::class,'store'])->middleware('guest')->name('resetPass');
+
+Route::get('/email/verify',function (Request $request){
+
+    if($request->user()->hasVerifiedEmail()) {
+        return redirect()->route('userAccount');
+    }
+
+    return view('verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}',function(EmailVerificationRequest $request){
+    if($request->user()->hasVerifiedEmail()) {
+        return redirect()->route('userAccount');
+    }
+
+    $request->fulfill();
+
+    return redirect()->route('userAccount');
+})->middleware(['auth','signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification',function (Request $request){
+
+    if($request->user()->hasVerifiedEmail()) {
+        return redirect()->route('userAccount');
+    }
+
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message','Verification link set!');
+})->middleware('auth')->name('verification.send');
+
+Route::view('/userAccount','userAccount')->middleware(['auth','verified'])->name('userAccount');
 
 Route::post('/exit', [LoginController::class,'exit'])->middleware('auth')->name('exit');
 
