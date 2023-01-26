779806063eca31f78bef27bda863563c06e0f1c7
deniskorbakov <korbakovd@gmail.com>
2023-01-27 00:58:07 +0300

    добавил раздел

--- resources/views/templates/doctypeHtml.blade.php
+++ resources/views/templates/doctypeHtml.blade.php
@@ -28,6 +28,9 @@
     @yield('background-js')
 @endif
 
+<script src="https://cdn.jsdelivr.net/npm/cdbootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/cdbootstrap/js/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/cdbootstrap/js/cdb.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
